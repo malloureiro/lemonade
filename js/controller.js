2ea88d43c7546a9a0fbb2b7941dbee9ebd77730c
Mariana Loureiro <ma.lloureiro@gmail.com>
2017-05-29 22:51:48 -0300

    Criando repositório

---
+++ js/controller.js
@@ -0,0 +1,285 @@
+app.factory('DataHolderModel', function() {
+    var model = {
+        userName: '',
+        userSurname: '',
+        userAddress: {
+            isValid: false,
+            formatted_address: '',
+            logradouro: '',
+            numero: '',
+            complemento: '',
+            bairro: '',
+            cidade: '',
+            estado: '',
+            pais: ''
+        },
+        ownership: '' // possible: rent/own
+    }
+
+    var map = {
+        autocomplete: null,
+        infowindow: null,
+        marker: null,
+        map: null
+
+    }
+
+    var DataHolderModel = {
+        model,
+        map
+    }
+
+    return DataHolderModel;
+});
+
+app.controller('appController', function($scope, DataHolderModel) {
+
+    $scope.model = DataHolderModel.model;
+    $scope.map = DataHolderModel.map;
+    $scope.errorMessage = "";
+    $scope.buttonDisabled = true;
+    $scope.meetsRequirement = false;
+
+    //checkButtonDisabled();
+
+    var pageId = $("section > .container-fluid").attr("id");
+
+    $(".back-icon").click(function() {
+        switch(pageId) {
+            case 'form-2':
+                window.location.href = "#start/1";
+                break;
+            case 'form-3':
+                window.location.href = "#start/2";
+                reInitializeMap();
+                break;
+            case 'form-4':
+                window.location.href = "#start/3";
+                break;
+            default:
+                return false;
+        }
+    });
+
+    $scope.validatePerson = function() {
+        var firstName = $("#firstName").val();
+        var lastName = $("#lastName").val();
+
+        if (firstName && lastName) {
+            if (firstName.length < 2 || lastName.length < 2 ) {
+                $scope.errorMessage = "Os nomes devem conter pelo menos 2 caracteres";
+                $(".error-message").show();
+                return;
+            }
+            window.location.href =  '#start/2';
+        } else {
+            $scope.errorMessage = "As informações devem ser preenchidas";
+            $(".error-message").show();
+            $("#firstName").addClass("form-control-focused");
+            $("#lastName").addClass("form-control-focused");
+        }
+    }
+
+    $scope.validateAddress = function() {
+        var locationInput = $("#locationInput").val();
+        var locationCompInput = $("#locationCompInput").val();
+
+        if (locationCompInput) {
+            $scope.model.userAddress.complemento = locationCompInput;
+        }
+        if (locationInput) {
+            if ($scope.model.userAddress.isValid) {
+                window.location.href =  '#start/3';    
+            } else {
+                // Adicionando validação em caso de evento 'place_changed' não disparado
+                $scope.errorMessage = "O endereço informado não é válido";
+                $(".error-message").show();
+            }
+        }
+    }
+
+    $scope.validateOwnership = function() {
+        var input = $("input[name='ownership']:checked").val();
+        if (input) {
+            $scope.model.ownership = input;
+            window.location.href = "#start/4";
+        }
+    }
+
+    $scope.validateOptionals = function() {
+        window.location.href = "#start/5";
+    }
+
+    $scope.validateValuables = function() {
+        window.location.href = "#start/6";
+    }
+
+    $scope.validateActivePolicy = function() {
+        window.location.href = "#start/7";
+    }
+
+    $scope.validatePropertyType = function() {
+        
+    }
+
+    /*
+        Esta verificação deveria ser realizada através de propriedades nativas do AngularJS. Exemplo: ng-disabled.
+    */
+    function checkButtonDisabled() {
+        /*debugger;
+        var target = ".start-buttons";
+        if ($scope.buttonDisabled && $scope.meetsRequirement) {
+            $(target).removeClass('disabled');
+            $scope.buttonDisabled = false;
+        }
+
+        var isDisabled = $(target).hasClass('disabled');
+        if (isDisabled && $scope.buttonDisabled) {
+            $(target).removeClass('disabled');
+            $scope.buttonDisabled = false;
+        }*/
+    }
+
+    // Evento no input text do Form 1
+    $(document).on("input", "input[name='input-person']", function() {
+        if ($(".error-message").is(":visible")) {
+            $(this).removeClass("form-control-focused");
+            return;
+        }
+        $(".error-message").hide();
+    });
+
+    // Evento no radio do Form 3
+    $(document).on("change", "input[name='ownership']", function() {
+        if ($("input[name='ownership']:checked").val()) {
+            $scope.meetsRequirement = true;
+            //checkButtonDisabled();
+        }
+    });    
+
+    function reInitializeMap() {
+    }
+
+});
+
+app.controller('mapController', function($scope, DataHolderModel) {
+
+    $scope.model = DataHolderModel.model;
+    $scope.user = $scope.model.userName;
+    $scope.mapModel = DataHolderModel.map;
+    $scope.errorMessage = "";
+
+    angular.element(document).ready(function () {
+        initMap();
+    });
+
+    function initMap() {
+
+        var map = new google.maps.Map(document.getElementById('map'));
+        var input = document.getElementById('locationInput');
+
+        var autocomplete = new google.maps.places.Autocomplete(input);
+        autocomplete.bindTo('bounds', map);
+
+        var infowindow = new google.maps.InfoWindow();
+        var marker = new google.maps.Marker({
+            map: map,
+            anchorPoint: new google.maps.Point(0, -20)
+        });
+
+        autocomplete.addListener('place_changed', function() {
+            
+            var place = autocomplete.getPlace();
+            if (!place.geometry) {
+                $('.container-map').hide();
+                $scope.errorMessage = "O endereço informado não é válido";
+                $(".error-message").show();
+
+                // Apply necessário dentro de event listener
+                $scope.$apply();
+                return;
+            } else {
+                $(".error-message").hide();
+                $scope.model.userAddress.formatted_address = place.formatted_address;
+                
+                if (place.address_components.length < 8) {
+                    $scope.errorMessage = "O endereço informado deve conter informações de número e bairro";
+                    $(".error-message").show();
+                    $("#locationInput").val('');
+
+                    $scope.$apply();
+                    return;
+                } else {
+                    $('.container-map').show();
+                    google.maps.event.trigger(map, 'resize');
+
+                    $(".error-message").hide();
+                    $scope.model.userAddress.logradouro = place.address_components[0].long_name;
+                    $scope.model.userAddress.numero = place.address_components[1].long_name;
+                    $scope.model.userAddress.bairro = place.address_components[2].long_name;
+                    $scope.model.userAddress.cidade = place.address_components[3].long_name;
+                    $scope.model.userAddress.estado = place.address_components[5].short_name;
+                    $scope.model.userAddress.pais = place.address_components[6].long_name;
+                    $scope.model.userAddress.cep = place.address_components[7].long_name;
+                    $scope.model.userAddress.isValid = true;
+
+                    // If the place has a geometry, then present it on a map.
+                    if (place.geometry.viewport) {
+                        map.fitBounds(place.geometry.viewport);
+                    } else {
+                        map.setCenter(place.geometry.location);
+                        map.setZoom(17);
+                    }
+                    marker.setIcon(({
+                        url: place.icon,
+                        size: new google.maps.Size(71, 71),
+                        origin: new google.maps.Point(0, 0),
+                        anchor: new google.maps.Point(17, 34),
+                        scaledSize: new google.maps.Size(35, 35)
+                    }));
+                    marker.setPosition(place.geometry.location);
+                    marker.setVisible(true);
+
+                    var address = '';
+                    if (place.address_components) {
+                        address = [
+                          (place.address_components[0] && place.address_components[0].short_name || ''),
+                          (place.address_components[1] && place.address_components[1].short_name || ''),
+                          (place.address_components[2] && place.address_components[2].short_name || '')
+                        ].join(' ');
+                    }
+
+                    infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+                    infowindow.open(map, marker);
+                }
+            }
+            $scope.mapModel.infowindow = infowindow;
+            $scope.mapModel.marker = marker;
+            $scope.mapModel.autocomplete = autocomplete;
+            $scope.mapModel.map = map;
+        });
+    }
+
+    /*
+        Eventos de Input para Mapa
+    */
+    function toggleClass (v) {
+        return v ? 'addClass' : 'removeClass';
+    }
+
+
+    $(document)
+    .on('input', '.clearable', function() {
+        $(this)[toggleClass(this.value)]('x');
+    })
+    .on('mousemove', '.x', function(e){
+        $(this)[toggleClass(this.offsetWidth-18 < e.clientX -this.getBoundingClientRect().left)]('onX');   
+    })
+    .on('click', '.onX', function() {
+        $(this).removeClass('x onX').val('');
+        $('.container-map').hide();
+        $(".error-message").hide();
+    });
+});
+  
+
---
+++ js/routes.js
@@ -0,0 +1,60 @@
+var app = angular.module('Lemonade', ['ngRoute']);
+
+app.config(['$routeProvider','$locationProvider', function ($routeProvider, $locationProvider) {
+    $routeProvider
+      .when('/', {
+       templateUrl: "views/institutional.html",
+       controller: 'appController'
+      })
+      .when('/start/1', {
+       templateUrl: "views/forms/form1_common.html",
+       controller: 'appController'
+      })
+      .when('/start/2', {
+       templateUrl: "views/forms/form2_common.html",
+       controller: 'appController'
+      })
+      .when('/start/3', {
+       templateUrl: "views/forms/form3_common.html",
+       controller: 'appController'
+      })
+      .when('/start/4', {
+       templateUrl: "views/forms/form4_common.html",
+       controller: 'appController'
+      })
+      .when('/start/5', {
+       templateUrl: "views/forms/form5_rent.html",
+       controller: 'appController'
+      })
+      .when('/start/6', {
+       templateUrl: "views/forms/form6_rent.html",
+       controller: 'appController'
+      })
+      .when('/start/7', {
+       templateUrl: "views/forms/form7_own.html",
+       controller: 'appController'
+      })
+      .when('/start/8', {
+       templateUrl: "views/forms/form8_own.html",
+       controller: 'appController'
+      })
+      .when('/start/9', {
+       templateUrl: "views/forms/form9_own.html",
+       controller: 'appController'
+      })
+      .when('/start/10', {
+       templateUrl: "views/forms/form10_own.html",
+       controller: 'appController'
+      })
+      .when('/start/11', {
+       templateUrl: "views/forms/form11_own.html",
+       controller: 'appController'
+      })
+      .when('/start/12', {
+       templateUrl: "views/forms/form12_own.html",
+       controller: 'appController'
+      })
+      .otherwise({ redirectTo: '/' });
+
+      $locationProvider.hashPrefix('');
+}]);
